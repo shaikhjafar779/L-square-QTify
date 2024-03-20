34edd8f58af6b0732454d43f230e10d38e62c201
Jafar Shaikh <jafar@datavoice.co.in>
2024-03-20 14:56:32 +0530

    hero section changes

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -4,13 +4,8 @@ import heroImage from "../../assets/hero.svg";
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div>
-        <h1>100 Thousand Songs, ad-free</h1>
-        <h1>Over thousands podcast episodes</h1>
-      </div>
-      <div>
-        <img src={heroImage} alt="Hero_Image" />
-      </div>
+      <h1>100 Thousand Songs, ad-free Over thousands podcast episodes</h1>
+      <img src={heroImage} alt="Hero_Image" />
     </div>
   );
 };
